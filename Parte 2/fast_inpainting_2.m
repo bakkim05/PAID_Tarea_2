7cffce871d464f38b96f9c288cd3c734cbf2829d
Dagoberto Rojas <krojas96ce@gmail.com>
2020-11-23 23:15:05 -0600

    updated

---
+++ Parte 2/fast_inpainting_2.m
@@ -0,0 +1,39 @@
+pkg load image;
+clc; clear; close all;
+A = imread('images/orig2.jpg');
+B = imread('images/objeto2.jpg');
+B = B <= 50;
+figure
+imshow(B)
+%disp(B(1:50, 1:50));
+%Algoritmo de recuperación
+
+function I = f_inp(A, M, bounds, its)
+  if(its > 0)
+    C = conv2(A, M, 'same');
+    A(bounds) = C(bounds);
+    I= f_inp(A, M, bounds, its-1);
+    return
+  endif
+  I = A;
+  return
+endfunction 
+
+a = 0.073235;
+b = 0.176765;
+%a = 0.125;
+%b = 0.125;
+M =  [a b a; 
+      b 0 b; 
+      a b a];
+bounds = B;
+A = im2double(A);
+
+its=255;
+%A = f_inp(A, M, bounds, its); 
+A(:,:,1) = f_inp(A(:,:,1), M, bounds, its);
+A(:,:,2) = f_inp(A(:,:,2), M, bounds, its);
+A(:,:,3) = f_inp(A(:,:,3), M, bounds, its);
+
+figure
+  imshow(im2uint8(A));
