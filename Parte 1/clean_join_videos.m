80a582aa2fb5dc563e40f0dba094e470ba659477
Jung <bakkim05@gmail.com>
2020-11-25 22:11:19 -0600

    final

---
+++ Parte 1/clean_join_videos.m
@@ -0,0 +1,45 @@
+%Packages
+pkg load video
+pkg load image
+
+%Load videos
+videoJung = VideoReader("videos/jung.mp4");
+videoDani = VideoReader("videos/daniel.mp4");
+videoDago = VideoReader("videos/dago.mp4");
+
+%Get frames from videos
+frameJung = videoJung.NumberOfFrames;
+frameDani = videoDani.NumberOfFrames;
+frameDago = videoDago.NumberOfFrames;
+
+%Create template for output video
+frameTotal = frameJung + frameDani + frameDago
+videoHeight = videoJung.Height;
+videoWidth = videoJung.Width;
+output = uint8(zeros(videoHeight, videoWidth, 3, frameTotal));
+
+%Merge Videos
+%%Video Jung
+for i = 1 : frameJung
+     frame = readFrame(videoJung);
+     output(:,:,:,i) = frame(:,:,:);
+end
+
+%%Video Daniel
+for i = frameJung+1 : frameJung + frameDani
+     frame = readFrame(videoDani);
+     output(:,:,:,i) = frame(:,:,:);
+end
+
+%%Video Dago
+for i = frameJung + frameDani + 1  : frameTotal
+     frame = readFrame(videoDago);
+     output(:,:,:,i) = frame(:,:,:);
+end
+
+%Escribir el video en el archivo
+videoOutput = VideoWriter("videos/video_sin_ruido.mp4");
+for i = 1 : frameTotal
+     writeVideo(videoOutput, output(:,:,:,i));
+end
+close(videoOutput)
